012c38f8970b4f37edd3f4f0b1ab1b1a237a607b
prezofbfclub <41440714+prezofbfclub@users.noreply.github.com>
2023-01-24 12:36:10 -0500

    Update streamlit_app.py
    build smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.header('🥣Breakfast Menu')
 streamlit.text('🥗Omega 3 and Blueberry Meal')
 streamlit.text('🐔Kale, Spinach and Rocket Smoothie')
 streamlit.text('🥑🍞Hard-Boiled and Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
