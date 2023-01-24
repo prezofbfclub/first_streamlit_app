8a3254252a5c16c23b81a384260c366d3dbacbb1
prezofbfclub <41440714+prezofbfclub@users.noreply.github.com>
2023-01-24 12:35:44 -0500

    Update streamlit_app.py
    icons1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and Blueberry Meal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-Boiled and Free-Range Egg')
+streamlit.header('🥣Breakfast Menu')
+streamlit.text('🥗Omega 3 and Blueberry Meal')
+streamlit.text('🐔Kale, Spinach and Rocket Smoothie')
+streamlit.text('🥑🍞Hard-Boiled and Free-Range Egg')
